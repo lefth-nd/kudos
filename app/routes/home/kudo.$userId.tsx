4cdbfbea139237ef74aad1916c75c091f36359f1
Jay <schu0527@flinders.edu.au>
2022-11-02 12:16:38 +1030

    added portal

--- app/routes/home/kudo.$userId.tsx
+++ app/routes/home/kudo.$userId.tsx
@@ -1,6 +1,7 @@
 import { json, LoaderFunction, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getUserById } from "~/utils/user.server";
+import { Portal } from "~/components/portal";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
     const { userId } = params
@@ -19,6 +20,9 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 export default function KudoModal() {
     const data = useLoaderData()
     console.log("data", data[0])
-    return <h2> User: {data.recipient.profile.firstName} {data.recipient.profile.lastName} </h2>
+    return <Portal wrapperId="kudo-modal">
+
+    <h2> User: {data.recipient.profile.firstName} {data.recipient.profile.lastName} </h2>
+    </Portal>
 }
 
